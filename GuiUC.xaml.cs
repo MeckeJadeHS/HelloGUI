f5d592e3655786b5f66d0d6ca175431572b2b54a
MeckeJadeHS <mecke@jade-hs.de>
2019-11-08 15:51:46 +0100

    Initial Upload
    Beispielcode aus Vorlesung 7

---
+++ GuiUC.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HalloGUI2
+{
+    /// <summary>
+    /// Interaktionslogik für GuiUC.xaml
+    /// </summary>
+    public partial class GuiUC : UserControl
+    {
+        public GuiUC()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btn_hide_Click(object sender, RoutedEventArgs e)
+        {
+            tb_wertx.Visibility = Visibility.Hidden;
+            lab_wertx.Content = "Knpof gedrückt!";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace HalloGUI2
+{
+    class Program
+    {
+        public Program()
+        {
+            Window fenster =new Window();
+            GuiUC myGUI = new GuiUC();
+
+            fenster.Content = myGUI;
+            fenster.Title = "Mein Fenster";
+            fenster.SizeToContent = SizeToContent.WidthAndHeight;
+            fenster.ResizeMode = ResizeMode.NoResize;
+
+            fenster.ShowDialog();
+
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+}
